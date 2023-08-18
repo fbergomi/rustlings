3ce2636f0a11559461b42796c2e16a2665d7857a
F Bergomi <bergomi.franco@gmail.com>
2023-08-18 23:32:26 +0200

    Error handling ok

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -19,16 +19,27 @@
 // Execute `rustlings hint errors2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 use std::num::ParseIntError;
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
 
+    //with short expression
+    //see : https://doc.rust-lang.org/book/ch09-02-recoverable-errors-with-result.html#a-shortcut-for-propagating-errors-the--operator
+    let qty = item_quantity.parse::<i32>()?;
     Ok(qty * cost_per_item + processing_fee)
+
+    //with match
+    /*
+    let qty = item_quantity.parse::<i32>();
+    match qty{
+        Err(err)=>Err(err),
+        Ok(qty)=>Ok(qty * cost_per_item + processing_fee) 
+    }
+    */
 }
 
 #[cfg(test)]
