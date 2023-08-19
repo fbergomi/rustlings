f3e7ed6c661817cb21792f28def215c3664a7b86
F Bergomi <bergomi.franco@gmail.com>
2023-08-19 17:09:19 +0200

    fun with traits

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -8,13 +8,47 @@
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
+impl AppendBar for Vec<String>{
+    fn append_bar(mut self) -> Vec<String> {
+        
+        self.push("Bar".to_string());
+
+        self
+
+        /*
+        let mut tmp:Vec<String> = Vec::new();
+
+        for element in self.iter() {
+            let mut tmpStr = String::new();
+            tmpStr.push_str(&element);
+            tmp.push(tmpStr.to_string());
+        }
+        tmp.push("Bar".to_string());
+        tmp
+        */
+
+        //i was appending "Bar" to each element of the input vector
+        /*
+        for element in self.iter() {
+            let mut tmpStr = String::new();
+            tmpStr.push_str(&element);
+            tmpStr.push_str("Bar");
+            
+            println!("{} --> {}",element,tmpStr);
+            tmp.push(tmpStr.to_string());
+        }
+        tmp
+        */
+        
+    }
+}
 
 #[cfg(test)]
 mod tests {
