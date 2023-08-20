088220290bc7942376b39e78b50a86b3753318d1
F Bergomi <bergomi.franco@gmail.com>
2023-08-20 18:22:33 +0200

    master of the iterators + fold / count !

--- exercises/iterators/iterators5.rs
+++ exercises/iterators/iterators5.rs
@@ -11,7 +11,7 @@
 // Execute `rustlings hint iterators5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 use std::collections::HashMap;
 
@@ -35,7 +35,8 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    map.iter().filter(|x|(*x.1==value)).count()
+   //todo!();
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -54,7 +55,14 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
-    todo!();
+    
+    //with fold
+    collection.iter().fold(0, |acc, current|acc + count_iterator(current, value))
+
+    //with sum
+    //collection.iter().map(|current| count_iterator(current, value)).sum()
+
+    //todo!();
 }
 
 #[cfg(test)]
