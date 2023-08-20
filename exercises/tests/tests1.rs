49db92bb43a674ec295ab23081d4a30faf71f88d
F Bergomi <bergomi.franco@gmail.com>
2023-08-20 17:58:10 +0200

    tests

--- exercises/tests/tests1.rs
+++ exercises/tests/tests1.rs
@@ -10,12 +10,13 @@
 // Execute `rustlings hint tests1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert() {
-        assert!();
+        assert_eq!(true,true,"good");
+        assert!(true,"bad");
     }
 }
