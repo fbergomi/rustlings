10ae2fc2351378131c9c3c207eac1ec59c3b15ca
F Bergomi <bergomi.franco@gmail.com>
2023-08-20 18:09:51 +0200

    iterators4 : factorial with fold (and closure)

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -6,7 +6,7 @@
 // I AM DONE
 
 pub fn factorial(num: u64) -> u64 {
-    // Complete this function to return the factorial of num
+    // Complete this function to return the factorial of numA
     // Do not use:
     // - return
     // Try not to use:
@@ -15,8 +15,14 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
-    
-    (1..=num).product()
+
+
+    //with fold (it uses an accumulator)
+    (1..=num).fold(1, |acc, x| acc * x)
+
+
+    //with product
+    //(1..=num).product()
 
     //with the usage of an iterator & associated funcs
     /*
