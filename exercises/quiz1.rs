15b6bc8d6437c6a281994c5e013c0a4d1d857474
bergomf <bergomi.franco@gmail.com>
2023-10-24 23:27:19 +0200

    Rustlings - Quizz 1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+// I AM DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(nb_apples: i32) -> i32 {
+    match nb_apples {
+        0..=40 => nb_apples * 2,
+        41.. => nb_apples,
+        _ => 0,
+    }
+}
 
 // Don't modify this function!
 #[test]
