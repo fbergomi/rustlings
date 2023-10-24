fac84c74ef7a7f217e966eb242b03f174597ad36
bergomf <bergomi.franco@gmail.com>
2023-10-24 22:45:15 +0200

    Rustlings - Functions

--- exercises/02_functions/functions3.rs
+++ exercises/02_functions/functions3.rs
@@ -3,14 +3,14 @@
 // Execute `rustlings hint functions3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
-    call_me();
+    call_me(5);
 }
 
 fn call_me(num: u32) {
-    for i in 0..num {
-        println!("Ring! Call number {}", i + 1);
+    for i in 1..=num {
+        println!("Ring! Call number {}", i);
     }
 }
