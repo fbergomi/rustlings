ce18c05f1f5aa8d77ef76e44c67f4c1b9df2a274
bergomf <bergomi.franco@gmail.com>
2023-11-04 00:09:15 +0100

    Rustlings - Hashmaps

--- exercises/11_hashmaps/hashmaps2.rs
+++ exercises/11_hashmaps/hashmaps2.rs
@@ -12,10 +12,7 @@
 // Make me pass the tests!
 //
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
+// hint.
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -40,6 +37,10 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        if !basket.contains_key(&fruit)
+        {
+            basket.insert(fruit,1);
+        }
     }
 }
 
