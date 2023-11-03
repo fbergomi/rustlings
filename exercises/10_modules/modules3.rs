4f10b4734b67fc5c5fd3fc0721274052ebbebf8c
bergomf <bergomi.franco@gmail.com>
2023-11-03 23:49:00 +0100

    Rustlings - Modules

--- exercises/10_modules/modules3.rs
+++ exercises/10_modules/modules3.rs
@@ -6,12 +6,9 @@
 // you can do it with one line!
 //
 // Execute `rustlings hint modules3` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
+// hint.
 // TODO: Complete this use statement
-use ???
+use std::time::{SystemTime, UNIX_EPOCH};
 
 fn main() {
     match SystemTime::now().duration_since(UNIX_EPOCH) {
