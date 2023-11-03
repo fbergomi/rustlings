974e23e161e42f6a39e25f24656d19e9b6013dd1
bergomf <bergomi.franco@gmail.com>
2023-11-03 22:35:33 +0100

    Rustlings - Strings

--- exercises/09_strings/strings1.rs
+++ exercises/09_strings/strings1.rs
@@ -3,15 +3,12 @@
 // Make me compile without changing the function signature!
 //
 // Execute `rustlings hint strings1` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
+// hint.
 fn main() {
     let answer = current_favorite_color();
     println!("My current favorite color is {}", answer);
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+    String::from("blue")
 }
