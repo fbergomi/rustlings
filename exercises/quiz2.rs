cc41cadc47de0f7b29c75ebcf4589f542bc6793a
bergomf <bergomi.franco@gmail.com>
2023-11-04 21:25:44 +0100

    Rustlings - Quizz 2

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,10 +18,7 @@
 //   the first element is the string, the second one is the command.
 // - The output element is going to be a Vector of strings.
 //
-// No hints this time!
-
-// I AM NOT DONE
-
+// No hints this time!
 pub enum Command {
     Uppercase,
     Trim,
@@ -32,11 +29,22 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output: Vec<String> = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            output.push(match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(nb) => {
+                    let mut new_string = String::from(string);
+                    for _i in 0..(*nb) {
+                        new_string.push_str("bar")
+                    }
+                    new_string
+                }
+            });
         }
         output
     }
@@ -45,8 +53,8 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
     use super::Command;
+    use crate::my_module::transformer;
 
     #[test]
     fn it_works() {
