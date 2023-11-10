9f7f5358bfa92000036f93be2721d46abb16c71a
Franco Bergomi <bergomi.franco@gmail.com>
2023-11-10 13:26:58 +0100

    Rustlings - Quiz 3

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,15 +16,30 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+use std::fmt::Display;
 
-pub struct ReportCard {
-    pub grade: f32,
+/* pub enum Grade {
+    F32Grade(f32),
+    StringGrade(String)
+} */
+
+/* impl Display for Grade{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self {
+            Grade::F32Grade(f32_grade) =>  write!(f, "{}", f32_grade),
+            Grade::StringGrade(string_grade) => write!(f, "{}",string_grade)
+        }
+    }
+} */
+
+
+pub struct ReportCard<T> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: Display> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
@@ -38,6 +53,7 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
+            //grade: Grade::F32Grade(2.1),
             grade: 2.1,
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
@@ -52,7 +68,8 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            //grade: Grade::StringGrade("A+".to_string()),
+            grade: "A+".to_string(),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
